7bebe327a1616b2bf984a7b3aa9b2d1b796a75e9
unknown <jeidersilgado@gmail.com>
2023-08-30 19:59:08 -0500

    actualizando my conexion mysql

--- dbase.py
+++ dbase.py
@@ -1,7 +1,7 @@
 import mysql.connector
 database = mysql.connector.connect(
     user='root',
-    host= 'db',
+    host= 'db:22',
     port='3306',
     password='#Jei123456',
     database='bd_usuarios')
